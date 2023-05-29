94f977e54c9715b42aa6f151b9361641a0f06884
alisonkim <alison.y.kim@berkeley.edu>
2023-05-29 18:56:21 +0200

    Upload scripts for part 2 (beam size experiments)

---
+++ scripts/modify_k.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# modify_k.py
+
+"""
+Modify model config with the specified beam size.
+
+Sample program call from main directory:
+	python3 scripts/modify_k.py --model transformer_b --beam-size 4
+"""
+
+import os
+import sys
+import yaml
+from argparse import ArgumentParser
+
+
+def get_argument_parser() -> ArgumentParser:
+	parser = ArgumentParser(description='Specify parameters for training.')
+	parser.add_argument(
+		'--model',
+		type=str, required=True,
+		help='Name of model with which to perform inference.'
+	)
+	parser.add_argument(
+		'--beam-size',
+		type=int, required=True,
+		help='Size of beam.'
+	)
+	return parser
+
+
+if __name__ == '__main__':
+	# Parse CL args
+	parser = get_argument_parser()
+	args = parser.parse_args()
+
+	# Define directories
+	EX_DIR = os.getcwd() # Should be exercise directory 'mt-2023-ex05'
+	MODELS_DIR = os.path.join(EX_DIR, 'configs')
+	MODEL_PATH = os.path.join(MODELS_DIR, args.model + '.yaml')
+
+	# Change YAML
+	try:
+		with open(MODEL_PATH) as config:
+			yaml_doc = yaml.safe_load(config)
+			yaml_doc['testing']['beam_size'] = args.beam_size
+		
+		MODEL_PATH_K = os.path.join(MODELS_DIR, args.model + '_' + str(args.beam_size) + '.yaml')
+		with open(MODEL_PATH_K, 'w') as config_k:
+			yaml.dump(yaml_doc, config_k, default_flow_style=False, sort_keys=False)
+	except FileNotFoundError:
+		print(f'Invalid model name {args.task}. Please try again with a valid model name, or train a model with this name and rerun this script.')
+		sys.exit()
---
+++ scripts/translate.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+data=$base/data
+configs=$base/configs
+translations=$base/translations
+translations_logs=$translations/logs
+
+mkdir -p $translations
+mkdir -p $translations_logs
+
+src=de
+trg=nl
+
+num_threads=4
+device=0
+
+model_name="transformer_c"
+
+beam_sizes="1 2 4 6 8 10 12 14 16 18"
+
+SECONDS=0
+
+for k in $beam_sizes; do
+	# Create temporary config file with specified beam size `k`
+	python scripts/modify_k.py --model $model_name --beam-size $k
+
+	# Define directory in which translations will be stored
+	translations_sub=$translations/$model_name
+	mkdir -p $translations_sub
+
+	# Perform inference
+	echo ""
+	echo "###############################################################################"
+	echo "now producing translations with model \"$model_name\", beam size $k"
+	echo "###############################################################################"
+
+	SECONDS_K=0
+	
+	CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name\_$k.yaml < $data/test.$src-$trg.$src > $translations_sub/hyps.$k.$trg
+	
+	# Delete first line: "sentence" is written to top of file, which messes up alignment for BLEU
+	sed -i '' '1d' $translations_sub/hyps.$k.$trg
+
+	# Calculate BLEU, write to log file
+	cat $translations_sub/hyps.$k.$trg | sacrebleu $data/test.$src-$trg.$trg > $translations_logs/$k.log
+
+	# Append time to log file
+	echo "Time taken (seconds): $SECONDS" >> $translations_logs/hyps.$k.log
+
+	# Delete temporary config file
+	rm $configs/$model_name\_$k.yaml
+done
